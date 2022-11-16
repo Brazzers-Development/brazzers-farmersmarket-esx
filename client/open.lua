431b0af3be60ac4f24d876104583e24357a90b0f
MannyOnBrazzers <mannygraphicdesign@gmail.com>
2022-11-16 01:24:35 -0500

    Added param notes

--- client/open.lua
+++ client/open.lua
@@ -1,7 +1,11 @@
+-- You can use this function if you want to add ClockHours to determine if the market is open or not
+-- or any other shit you want to add inside this function
 function isMarketOpen()
     return true
 end
 
+-- @param msg - locale string
+-- @param type - 'error' / 'success'
 function notification(msg, type)
     ESX.ShowNotification(TranslateCap(msg), type)
 end
--- server/open.lua
+++ server/open.lua
@@ -1,6 +1,11 @@
-function notification(src, msg, type, param)
+
+-- @param src - players source
+-- @param msg - locale string
+-- @param type - 'error' / 'success'
+-- @param value - if the locale has a value, we push it through this param
+function notification(src, msg, type, value)
     local Player = ESX.GetPlayerFromId(src)
 
-    if param then return Player.showNotification(TranslateCap(msg, param), type) end
+    if value then return Player.showNotification(TranslateCap(msg, value), type) end
     Player.showNotification(TranslateCap(msg), type)
 end
